1ab1297f35e9bd86483a224adc7136678b206310
Eduard Fugarolas <efugarolas@brox.de>
2024-02-12 13:49:59 +0100

    Service Parser.

--- src/main/scala/Parsers.scala
+++ src/main/scala/Parsers.scala
@@ -15,9 +15,24 @@ object Parsers {
     )
   }
 
+  def service(line: String): Service = {
+    val Patterns.Services.line(id, name, products, productManager, price) = line
+    Service(
+      serviceId = id,
+      serviceName = name,
+      products = products,
+      productManager = productManager,
+      price = price
+    )
+  }
+
   def main(args: Array[String]): Unit = {
-    val line = "I241-8776317,Strain Compensator,12,68,15,8,Baldwin.Dirksen@company.org,\"0,50 EUR\""
-    val product: Product = Parsers.product(line)
+    val productLine = "I241-8776317,Strain Compensator,12,68,15,8,Baldwin.Dirksen@company.org,\"0,50 EUR\""
+    val product: Product = Parsers.product(productLine)
     println(product)
+
+    val serviceLine = "Y704-9764759,Product Analysis,\"O491-3823912, I965-1821441, Z655-3173353, U733-5722614, K411-1729714\",Lambert.Faust@company.org,\"748,40 EUR\""
+    val service: Service = Parsers.service(serviceLine)
+    println(service)
   }
 }
---
+++ src/main/scala/Service.scala
@@ -0,0 +1,9 @@
+package com.edufuga.scala.streaming
+
+case class Service(
+  serviceId: String,
+  serviceName: String,
+  products: String,
+  productManager: String,
+  price: String
+)
