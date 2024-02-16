6df0b62b80ad6a804d8cf0a3f2af868dd6905428
Eduard Fugarolas <efugarolas@brox.de>
2024-02-16 09:57:43 +0100

    Exclude empty lines.

--- data/src/main/scala/com/edufuga/scala/data/access/ops/StreamOps.scala
+++ data/src/main/scala/com/edufuga/scala/data/access/ops/StreamOps.scala
@@ -6,6 +6,7 @@ object StreamOps {
   def entitiesParser[F[_], E](toMaybeEntity: String => Option[E]): Pipe[F, Byte, Option[E]] =
     _.through(text.utf8.decode)
       .through(text.lines)
+      .filterNot(_.isEmpty)
       .drop(1)
       .map(toMaybeEntity)
 }
