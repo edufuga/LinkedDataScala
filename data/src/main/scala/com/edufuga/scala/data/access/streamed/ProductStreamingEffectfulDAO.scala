f14876cb199eb0d474b2371fbdb39be0f64b66b8
Eduard Fugarolas <efugarolas@brox.de>
2024-02-20 12:01:10 +0100

    Better names.

--- data/src/main/scala/com/edufuga/scala/data/access/streamed/ProductStreamingEffectfulDAO.scala
+++ data/src/main/scala/com/edufuga/scala/data/access/streamed/ProductStreamingEffectfulDAO.scala
@@ -5,5 +5,6 @@ import com.edufuga.scala.core.Identifiable
 import com.edufuga.scala.data.access.entities.ProductDAO
 import fs2.Stream
 
-type StreamIOWrapper[+O] = Stream[IO, O]
-trait ProductStreamingEffectfulDAO[Id, +F[+_], +O <: Identifiable[Id]] extends ProductDAO[StreamIOWrapper]
+type StreamingEffectfulWrapper[+O] = Stream[IO, O]
+
+trait ProductStreamingEffectfulDAO[Id, +O <: Identifiable[Id]] extends ProductDAO[StreamingEffectfulWrapper]
