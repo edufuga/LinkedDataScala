c16a030568cbd5e7bb0ddd6d4609e839e43f7fec
Eduard Fugarolas <efugarolas@brox.de>
2024-02-20 12:18:13 +0100

    DAOs with wrapper types are now just type aliases instead of a type hierarchy. MUCH cleaner.

--- data/src/main/scala/com/edufuga/scala/data/access/entities/DAOTypes.scala
+++ data/src/main/scala/com/edufuga/scala/data/access/entities/DAOTypes.scala
@@ -1,11 +1,17 @@
 package com.edufuga.scala.data.access.entities
 
 import cats.effect.IO
+import com.edufuga.scala.core._
+import com.edufuga.scala.core.ProductTypes.ProductId
+import com.edufuga.scala.core.ServiceTypes.ServiceId
+import com.edufuga.scala.data.access.DAO
 import fs2.Stream
 
 // Higher-kinded type for adapting from two type parameters (of Stream) to only one type parameter (of our entity DAOs).
 type StreamingEffectfulWrapper[+O] = Stream[IO, O]
 
+type ProductDAO[+W[+_]] = DAO[ProductId, W[Product]]
 type ProductStreamingEffectfulDAO = ProductDAO[StreamingEffectfulWrapper]
 
+type ServiceDAO[+W[+_]] = DAO[ServiceId, W[Service]]
 type ServiceStreamingEffectfulDAO = ServiceDAO[StreamingEffectfulWrapper]
