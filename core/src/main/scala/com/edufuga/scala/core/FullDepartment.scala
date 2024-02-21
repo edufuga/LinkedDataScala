a18211a326e8e3f5afb9a6de95c43fa19bde042e
Eduard Fugarolas <efugarolas@brox.de>
2024-02-21 15:09:05 +0100

    This is a nightmare. The list of products and services in the organization is still empty

--- core/src/main/scala/com/edufuga/scala/core/FullDepartment.scala
+++ core/src/main/scala/com/edufuga/scala/core/FullDepartment.scala
@@ -1,14 +1,12 @@
 package com.edufuga.scala.core
 
 import com.edufuga.scala.core.DepartmentTypes.{DepartmentId, DepartmentName}
-import com.edufuga.scala.core.ProductTypes.ProductId
-import com.edufuga.scala.core.ServiceTypes.ServiceId
 
 case class FullDepartment(
   id: DepartmentId,
   name: DepartmentName,
   manager: Manager,
   employees: List[Employee],
-  productIds: List[ProductId],
-  serviceIds: List[ServiceId]
+  products: List[Product],
+  services: List[Service]
 ) extends Identifiable[DepartmentId]
--- src/main/scala/com/edufuga/scala/streaming/Streamer.scala
+++ src/main/scala/com/edufuga/scala/streaming/Streamer.scala
@@ -1,7 +1,8 @@
 package com.edufuga.scala.streaming
 
-import com.edufuga.scala.core.Organisation
 import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+import com.edufuga.scala.core.{Department, FullDepartment, FullOrganisation, Organisation, Product, Service}
 import com.edufuga.scala.core.ProductTypes.ProductId
 import com.edufuga.scala.core.ServiceTypes.ServiceId
 import com.edufuga.scala.data.access.entities.{ProductStreamingEffectfulDAO, ServiceStreamingEffectfulDAO}
@@ -16,30 +17,65 @@ class Streamer(
   serviceDAO: ServiceStreamingEffectfulDAO,
   organisationDAO: MaterializingOrganisationDAO
 ) {
+  def toEvalFullDepartment(department: Department): IO[FullDepartment] = {
+    val productsEval: IO[List[Product]] = productDAO.readByIds(department.productIds).compile.toList
+    val servicesEval: IO[List[Service]] = serviceDAO.readByIds(department.serviceIds).compile.toList
+
+    val productsAndServicesEval: IO[(List[Product], List[Service])] = IO.both(productsEval, servicesEval)
+
+    val fullDepartmentEval: IO[FullDepartment] = productsAndServicesEval.map { (products, services) =>
+      FullDepartment(
+        department.id,
+        department.name,
+        department.manager,
+        department.employees,
+        products,
+        services
+      )
+    }
+
+    fullDepartmentEval
+  }
+  def toFullOrganisation(organisation: Organisation): IO[FullOrganisation] = {
+    val fullDepartmentEvalList: List[IO[FullDepartment]] = organisation.departments.map(toEvalFullDepartment)
+
+    // Convert the List[IO[...]] to IO[List[...]]
+    val evalFullDepartments: IO[List[FullDepartment]] = fullDepartmentEvalList.sequence
+
+    val evalFullOrganisation: IO[FullOrganisation] = evalFullDepartments.map { fullDepartments =>
+      FullOrganisation(fullDepartments)
+    }
+
+    evalFullOrganisation
+  }
+
   def stream: IO[ExitCode] = {
     for {
-      _ <- IO.println("Processing 'products.csv', 'services.csv' and 'orgmap.xml'.")
+      //_ <- IO.println("Processing 'products.csv', 'services.csv' and 'orgmap.xml'.")
 
-      _ <- IO.println(s"Processing stream of products.")
+      //_ <- IO.println(s"Processing stream of products.")
       products <- productDAO.readAll.compile.toList
-      _ <- IO.println(products)
+      //_ <- IO.println(products)
 
-      _ <- IO.println(s"Processing stream of services.")
+      //_ <- IO.println(s"Processing stream of services.")
       services <- serviceDAO.readAll.compile.toList
-      _ <- IO.println(services)
+      //_ <- IO.println(services)
 
       _ <- IO.println("Processing the organisation file 'orgmap.xml'")
       organisation = organisationDAO.readAll
       _ <- IO.println(organisation)
 
-      _ <- IO.println(s"Finding a product by ID within the stream of products.")
+      _ <- IO.println("Processing the full organisation...")
+      fullOrganisation <- organisation.map(toFullOrganisation).sequence
+      _ <- IO.println(fullOrganisation)
+
+      // _ <- IO.println(s"Finding a product by ID within the stream of products.")
       bingoProduct <- productDAO.readById(ProductId("X716-6172862")).compile.last
-      _ <- IO.println("Bingo product: " + bingoProduct)
+      //_ <- IO.println("Bingo product: " + bingoProduct)
 
-      _ <- IO.println(s"Finding a service by ID within the stream of services.")
+      //_ <- IO.println(s"Finding a service by ID within the stream of services.")
       bingoService <- serviceDAO.readById(ServiceId("Y274-1029755")).compile.last
-      _ <- IO.println("Bingo service: " + bingoService)
-
+      // _ <- IO.println("Bingo service: " + bingoService)
     } yield ExitCode.Success
   }
 }
