b37e13f796f757bb6dbbc9f39646443028df770f
Eduard Fugarolas <efugarolas@brox.de>
2024-02-23 12:09:48 +0100

    Remove commented WIP code

--- data/src/main/scala/com/edufuga/scala/data/access/entities/DAOTypes.scala
+++ data/src/main/scala/com/edufuga/scala/data/access/entities/DAOTypes.scala
@@ -20,12 +20,6 @@ type FullOrganisationReader[+W[+_]] = ReadAll[FullOrganisation, W] // This wrapp
 type EffectfulStream[+S[+_[_], +_], EF[+_], +O] = S[EF, O] // This is a higher-kinded type alias, meant for streaming.
 type EffectfulOptional[EF[+_], +O] = EF[Option[O]] // This is just a simple type alias for effects around optionals.
 
-//type Ch[+_[_], +_] // ABSTRACT TYPE MEMBER
-//type Eff[+_]       // ABSTRACT TYPE MEMBER 
-//type St[+_] = EffectfulStream[Ch, Eff, _] // ABSTRACT TYPE MEMBERS (PARAMETERS)
-//type StrDAO[I, C[+_[_], +_], +F[+_], +O] = DAO[I, O, St] // ABSTRACT TYPE MEMBERS (PARAMETERS)
-// type ChannelingEffectfulDAO[I, C[+_[_], +_], +F[+_], +O]= DAO[I, O, EffectfulStream[C, F, _]] // Compile error (!!!)
-
 /* HOW + technology specific (e.g. TypeLevel stack types for Stream, IO, etc.) */
 // Higher-kinded type for adapting from two type parameters (of Stream) to only one type parameter (of our entity DAOs).
 type TypeLevelEffectfulStream[+O] = EffectfulStream[Stream, IO, O] // = Stream[IO, O]
