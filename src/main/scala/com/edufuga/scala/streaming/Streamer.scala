3f7fd04577e54f8875e340a848c39dfb51b95fea
Eduard Fugarolas <efugarolas@brox.de>
2024-02-26 12:25:55 +0100

    Separation of Streamer and StreamerApp. Notice the comments, regarding the usage of IO and Streams in such a higher-level.

--- src/main/scala/com/edufuga/scala/streaming/Streamer.scala
+++ src/main/scala/com/edufuga/scala/streaming/Streamer.scala
@@ -1,17 +1,12 @@
 package com.edufuga.scala.streaming
 
-import cats.effect.{ExitCode, IO, IOApp}
-import cats.implicits.*
-import com.edufuga.scala.entities.*
+import cats.effect.{ExitCode, IO}
 import com.edufuga.scala.entities.ProductTypes.ProductId
 import com.edufuga.scala.entities.ServiceTypes.ServiceId
-import com.edufuga.scala.operations.entity.implementation.EntityOperationImplementationTypes.*
-import com.edufuga.scala.operations.entity.implementation.effectful.FullOrganisationTypeLevelEffectfulCombinationDAO
-import com.edufuga.scala.operations.entity.implementation.materialized.file.FileMaterializingOrganisationDAO
-import com.edufuga.scala.operations.entity.implementation.streamed.file.*
+import com.edufuga.scala.operations.entity.implementation.EntityOperationImplementationTypes.{FullOrganisationTypeLevelEffectfulDAO, OrganisationMaterializedDAO, ProductTypeLevelEffectfulStreamingDAO, ServiceTypeLevelEffectfulStreamingDAO}
 
-import scala.util.{Failure, Success, Try}
-
+// Notice that this Streamer is still quite implementation (TypeLevel) specific.
+// Both the interface (parameters) and the implementation are full of IO and Streams and stuff.
 class Streamer(
   productDAO: ProductTypeLevelEffectfulStreamingDAO,
   serviceDAO: ServiceTypeLevelEffectfulStreamingDAO,
@@ -50,38 +45,5 @@ class Streamer(
       organisation <- fullOrganisationDAO.readAll
       _ <- IO.println(organisation)
     } yield ExitCode.Success
-  }
-}
-
-// TODO: Inject the FUNCTIONS that will create the (specific) DAOs in the Streamer!
-// The Streamer should NOT CREATE stuff by itself IN THIS HARD-CODED WAY!
-// WE DON'T WANT TO KNOW THE SUBTYPES SUCH AS FullOrganisationTypeLevelEffectfulCombinationDAO
-// This is similar to the _previous_ Streamer, but it should be the "StreamerApp" _class_.
-object Streamer extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] = {
-    Try {
-      val products: String = args(0)
-      val services: String = args(1)
-      val organisation: String = args(2)
-      (products, services, organisation)
-    } match
-      case Failure(_) => IO {
-        ExitCode.Error
-      }
-      case Success(p, s, o) =>
-        val productsDAO: ProductTypeLevelEffectfulStreamingDAO = ProductFileStreamingWithIODAO(p)
-        val servicesDAO: ServiceTypeLevelEffectfulStreamingDAO = ServiceFileStreamingWithIODAO(s)
-
-        val productsFromIds: List[ProductId] => IO[List[Product]] =
-          productIds => productsDAO.readByIds(productIds).compile.toList
-        val servicesFromIds: List[ServiceId] => IO[List[Service]] =
-          serviceIds => servicesDAO.readByIds(serviceIds).compile.toList
-
-        val organisationDAO: OrganisationMaterializedDAO = FileMaterializingOrganisationDAO(o)
-        val fullOrganisationDAO: FullOrganisationTypeLevelEffectfulDAO =
-          new FullOrganisationTypeLevelEffectfulCombinationDAO(productsFromIds, servicesFromIds, organisationDAO)
-
-        val streamer: Streamer = new Streamer(productsDAO, servicesDAO, organisationDAO, fullOrganisationDAO)
-        streamer.stream
   }
 }
