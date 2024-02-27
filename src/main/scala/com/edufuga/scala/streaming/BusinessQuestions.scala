1cb857b45d82dca75b19fcebbc03aaec202d5744
Eduard Fugarolas <efugarolas@brox.de>
2024-02-27 11:15:14 +0100

    Question that needs clarifying.

--- src/main/scala/com/edufuga/scala/streaming/BusinessQuestions.scala
+++ src/main/scala/com/edufuga/scala/streaming/BusinessQuestions.scala
@@ -1,6 +1,7 @@
 package com.edufuga.scala.streaming
 
 import cats.effect.{ExitCode, IO}
+import com.edufuga.scala.entities.Manager
 import com.edufuga.scala.operations.entity.implementation.EntityOperationImplementationTypes.*
 
 class BusinessQuestions(
@@ -11,6 +12,9 @@ class BusinessQuestions(
       _ <- IO.println("Processing the full organisation. This includes resolving the linked products and services.")
       organisation <- fullOrganisationDAO.readAll
       _ <- IO.println(organisation)
+
+      // Find services without a product manager
+      // Wait a second, the "Product" and "Service" have a MANDATORY ProductManager.
     } yield ExitCode.Success
   }
 }
