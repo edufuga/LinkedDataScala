23c8eae973f186f31b06bde578429304963cd059
Eduard Fugarolas <efugarolas@brox.de>
2024-03-15 10:57:11 +0100

    I am so proud (irony).

--- object_graph_mapping/src/main/scala/com/edufuga/scala/ogm/ObjectOntologyMappings.scala
+++ object_graph_mapping/src/main/scala/com/edufuga/scala/ogm/ObjectOntologyMappings.scala
@@ -6,9 +6,18 @@ import productdata.rdf.model as ont
 object ObjectOntologyMappings {
   private val NAMESPACE: String = "https://github.com/edufuga/LinkedDataScala/2024/3/ProductData#"
 
+  // SMELL: Using an obviously stateful counter for setting the "instance IDs" of the ontology objects.
+  //  A better solution would be to create the instance ID from a certain property of the objects themselves, but well.
+  private class Counting(val countableThing: String) {
+    private var counter: Integer = 1 // I hate my life.
+    def count(): String = s"${countableThing}_${counter += 1}" // I hate my life.
+  }
+
   object OrganisationMappings extends ObjectOntologyMapping[ent.FullOrganisation, ont.IOrganisation] {
+    private val COUNTER = Counting("organisation")
+
     override def objectToOntology(entity: ent.FullOrganisation): ont.IOrganisation = {
-      val ontology = ont.Organisation(NAMESPACE, s"$entity")
+      val ontology = ont.Organisation(NAMESPACE, COUNTER.count())
       entity.departments.foreach { department =>
         ontology.addDepartments(
           DepartmentMappings.objectToOntology(department)
