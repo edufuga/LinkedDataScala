fa3d570961da26e707b9b4347c93d85db8564396
Eduard Fugarolas <efugarolas@brox.de>
2024-03-15 10:20:54 +0100

    Template for organisation mapping.

--- object_graph_mapping/src/main/scala/com/edufuga/scala/ogm/ObjectOntologyMappings.scala
+++ object_graph_mapping/src/main/scala/com/edufuga/scala/ogm/ObjectOntologyMappings.scala
@@ -4,6 +4,12 @@ import com.edufuga.scala.entities._
 import productdata.rdf.model._
 
 object ObjectOntologyMappings {
+  object OrganisationMappings extends ObjectOntologyMapping[FullOrganisation, IOrganisation] {
+    override def objectToOntology(entity: FullOrganisation): IOrganisation = ???
+
+    override def ontologyToObject(ontology: IOrganisation): FullOrganisation = ???
+  }
+
   object DepartmentMappings extends ObjectOntologyMapping[FullDepartment, IDepartment] {
     override def objectToOntology(entity: FullDepartment): IDepartment = ???
     override def ontologyToObject(ontology: IDepartment): FullDepartment = ???
