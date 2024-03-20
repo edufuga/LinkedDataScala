6475c9f0dcd5d086b95db86f0e693b7667568780
Eduard Fugarolas <efugarolas@brox.de>
2024-03-20 13:41:31 +0100

    Graph to object injection with default value.

--- entity_operations_implementation/src/main/scala/com/edufuga/scala/operations/entity/implementation/effectful/graph/FullOrganisationTypeLevelEffectfulGraphDAO.scala
+++ entity_operations_implementation/src/main/scala/com/edufuga/scala/operations/entity/implementation/effectful/graph/FullOrganisationTypeLevelEffectfulGraphDAO.scala
@@ -2,13 +2,14 @@ package com.edufuga.scala.operations.entity.implementation.effectful.graph
 
 import cats.effect.IO
 import com.edufuga.scala.entities.FullOrganisation
+import com.edufuga.scala.ogm.ObjectGraphMappings
 import com.edufuga.scala.operations.entity.implementation.EntityOperationImplementationTypes.FullOrganisationTypeLevelEffectfulDAO
 import com.edufuga.scala.operations.entity.implementation.TechnologicalDetailTypes.TypeLevelEffectfulOptional
 import productdata.rdf.model.IOrganisation
 
 sealed class FullOrganisationTypeLevelEffectfulGraphDAO(
   graph: () => IOrganisation,
-  graphToObject: IOrganisation => FullOrganisation
+  private val graphToObject: IOrganisation => FullOrganisation = ObjectGraphMappings.OrganisationMappings.graphToObject
 ) extends FullOrganisationTypeLevelEffectfulDAO {
   override def readAll: IO[Option[FullOrganisation]] = {
     val organisationGraph: IOrganisation = graph()
