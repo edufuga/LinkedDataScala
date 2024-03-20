cf8720d5b7905d8bb5c31e0906694aafb911a711
Eduard Fugarolas <efugarolas@brox.de>
2024-03-20 15:10:01 +0100

    There is an error in the conversion, which needs to be corrected. But the code should not crash like it does.

--- entity_operations_implementation/src/main/scala/com/edufuga/scala/operations/entity/implementation/effectful/graph/FullOrganisationTypeLevelEffectfulGraphDAO.scala
+++ entity_operations_implementation/src/main/scala/com/edufuga/scala/operations/entity/implementation/effectful/graph/FullOrganisationTypeLevelEffectfulGraphDAO.scala
@@ -6,6 +6,8 @@ import com.edufuga.scala.ogm.ObjectGraphMappings
 import com.edufuga.scala.operations.entity.implementation.EntityOperationImplementationTypes.FullOrganisationTypeLevelEffectfulDAO
 import productdata.rdf.model.IOrganisation
 
+import scala.util.{Failure, Success, Try}
+
 sealed class FullOrganisationTypeLevelEffectfulGraphDAO(
   graph: () => IOrganisation, // XXX: When injecting this function, we need to pay special attention with the mutations!
 ) extends FullOrganisationTypeLevelEffectfulDAO {
@@ -13,9 +15,15 @@ sealed class FullOrganisationTypeLevelEffectfulGraphDAO(
     ObjectGraphMappings.OrganisationMappings.graphToObject(graph)
 
   override def readAll: IO[Option[FullOrganisation]] = {
-    val organisationGraph: IOrganisation = graph()
-    val fullOrganisation: FullOrganisation = graphToObject(organisationGraph)
-    // FIXME: We should optimize this into a lazy loading strategy, by wrapping the previous code inside an IO.
-    IO.pure(Some(fullOrganisation))
+    Try {
+      val organisationGraph: IOrganisation = graph()
+      val fullOrganisation: FullOrganisation = graphToObject(organisationGraph)
+      // FIXME: We should optimize this into a lazy loading strategy, by wrapping the previous code inside an IO.
+      fullOrganisation
+    } match
+      case Failure(exception) =>
+        println("Error! " + exception.getMessage)
+        IO.pure(None)
+      case Success(organisation) => IO.pure(Some(organisation))
   }
 }
